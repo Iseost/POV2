95481bdab244a0279f66a44d9ec3e5a5e760ef15
Iselin Engen <iselin.osterhus@gmail.com>
2025-12-15 06:34:20 +0100

    update footer
    Update Footer - adjust padding and background color

--- my-project/src/components/layout/Footer.jsx
+++ my-project/src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="rounded-t-mg shadow-[inset_0_-4px_10px_rgba(0,0,0,0.25)] p-4 mt-20 text-[var(--text-sub)] bg-[var(--bg-header)]">
+    <footer className="rounded-t-mg shadow-[inset_0_-4px_10px_rgba(0,0,0,0.25)] p-10 bg-white">
       <div className="container mx-auto text-center">
         &copy; {new Date().getFullYear()} Iselin Engen. All rights reserved.
       </div>
